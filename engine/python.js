add9f221c6837f07b5ba175431bc2ca5449d8196
Hamzah Nizami <hnizami1@stevens.edu>
2022-01-08 22:47:40 -0500

    Executed a single file

--- engine/python.js
+++ engine/python.js
@@ -1,8 +1,16 @@
 const fs = require('fs');
-//const exec = require('child_process').exec;
+const execute = require('./execCommand').execute;
 
-const runSingleFile = (file, file_name) => {
+const runSingleFile = async(file, file_post_data) => {
   console.log('Running single file...');
+
+  let file_name = file_post_data.file_name;
+  let time_limit = parseInt(file_post_data.time_limit);
+
+  //might need to check that there is .py at the end?
+  if (file_name.slice(file_name.length - 3) !== '.py') {
+      file_name += '.py';
+  }
 
   fs.writeFile(file_name, file, function(err) {
       if (err) {
@@ -12,11 +20,20 @@ const runSingleFile = (file, file_name) => {
       }
   });
 
-  return {
-      'message': 'Success',
-  };
+  const command = 'python3 ' + file_name;
+  const result = await execute(command, time_limit);
+  
+  fs.unlink(file_name, (err) => {
+      if (err) {
+          if (result.errors) {
+              result.errors.push('Error in removing file');
+          } else {
+              result.error = 'Error in removing file.';
+          }
+      }
+  });
 
-
+  return result;
 };
 
 module.exports = {
