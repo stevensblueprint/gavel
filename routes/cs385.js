341cc738391a3db944d90f09c9550c97beb11c9b
Hamzah Nizami <hnizami1@stevens.edu>
2022-01-12 21:01:32 -0500

    wrote the route for single file cpp

--- routes/cs385.js
+++ routes/cs385.js
@@ -1,5 +1,56 @@
 const express = require('express');
 const router = express.Router();
+const checkPost = require('../helpers/check_post_info');
+const aws = require('../data/utilAWS');
+
+router.post('/execute/single-test', async(req, res) => {
+   /*
+    POST data should look like this:
+
+    {
+        "class": "cs385",
+        "language": "cpp",
+        "file_name": "<FILE NAME AS AWS S3 KNOWS IT>",
+        "test_file_name": "<FILE NAME AS AWS S3 KNOWS IT>"
+        "time_limit": "10000",          **in milliseconds**
+        "memory_limit": "65536",        **in bytes**
+    }
+    */
+});
+
+router.post('/execute/single-file', async (req, res) => {
+      /*
+    POST data should look like this:
+
+    {
+        "class": "cs385",
+        "language": "cpp",
+        "compiler": "-g"       ** could also be -03**
+        "file_name": "<FILE NAME AS AWS S3 KNOWS IT>",
+        "time_limit": "10000",          **in milliseconds**
+        "memory_limit": "65536",        **in bytes**
+    }
+    */
+    const postDetails = req.body;
+    const errors = checkPost.checkSingleFilePost('cs385', postDetails);
+    if (errors.length > 0) {
+        return res.json({
+            'error': errors,
+        });
+    }
+
+    const getFile = await aws.retrieveFile(postDetails.file_name);
+    if (getFile.error) {
+        return res.json({
+            'error': getFile.error,
+        });
+    }
+
+    const file = getFile.file;
+    let output = await cppEngine.runSingleFile(file, postDetails);
+    res.json(output);
+
+});
 
 
 module.exports = router;
